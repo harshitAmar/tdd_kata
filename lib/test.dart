315a760bf95d6e9e8c9d658662ac14ca8d0729ab
Amarjeet Srivastava <lalaharshitamar@gmail.com>
2025-07-17 18:03:22 +0530

    first test without any logic in add function

--- lib/test.dart
+++ lib/test.dart
@@ -1,1 +1,14 @@
+import 'package:tdd_kata/calculator.dart';
+import 'package:test/test.dart';
 
+void main() {
+  group(
+    "Calculator",
+    () {
+      test("Handle Empty String", () {
+        Calculator calculator = Calculator();
+        expect(calculator.add(""), equals(0));
+      });
+    },
+  );
+}
